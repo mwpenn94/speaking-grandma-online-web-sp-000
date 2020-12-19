2934a5b040695d61a5883ab05847c44a670069ed
Michael Penn <mwpenn94@gmail.com>
2020-12-19 02:57:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,10 +14,10 @@
 
 def speak_to_grandma(phrase)
     if phrase == phrase.upcase
-      return = "NO, NOT SINCE 1938!"
-    elsif phrase = "I LOVE YOU GRANDMA!"
-      return = "I LOVE YOU TOO PUMPKIN!"
+      return "NO, NOT SINCE 1938!"
+    elsif phrase == "I LOVE YOU GRANDMA!"
+      return "I LOVE YOU TOO PUMPKIN!"
     else 
-      return = "HUH SPEAK UP, SONNY!"
+      return "HUH SPEAK UP, SONNY!"
     end
 end
